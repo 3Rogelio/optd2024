d2f1e81f915e6fff806d8f29dd215bcce6b72797
3Rogelio <99505188+3Rogelio@users.noreply.github.com>
2024-02-18 21:28:23 -0600

    Create test.jl

---
+++ test.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.12.18
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 5b2ee40e-a2b8-11ea-0fef-c35fe6918860
+md"""
+# Laboratorio 7 de Optimización Dinámica
+A continuación se muestran varios problemas de optimización dinámica
+"""
